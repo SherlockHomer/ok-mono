d3e54d99d33e4d57b790c7ffc397c7627a839cff
clark-cui <cuirongchuan@163.com>
2024-11-12 17:01:29 +0800

    fix: open connect modal ui change

--- packages/dapp-connect-sdk/src/ui/ConnectModal/index.ts
+++ packages/dapp-connect-sdk/src/ui/ConnectModal/index.ts
@@ -8,17 +8,19 @@ export function openConnectModal() {
   const wallets = getSupportWalletList();
   const closed = van.state(false);
   const formDom = div(
-    h3(" Connect Wallet"),
+    h3({style:"font-size:24px;"},
+        "Connect Wallet"
+        ),
     div(
       {
         style:
-          "display: flex; flex-direction: column; padding-top: 15px;min-width: 280px",
+          "display: flex; flex-direction: column; padding-top: 15px;min-width: 280px;font-size:15px;",
       },
       wallets.map((wallet) =>
         div(
           {
             style:
-              "display: flex; justify-content: space-between; align-items:center; padding-bottom:10px;",
+              "display: flex; justify-content: space-between; align-items:center; padding-bottom:13px;",
           },
           p(wallet.name),
           button(
@@ -27,7 +29,7 @@ export function openConnectModal() {
                 closed.val = true;
                 connectCallBack(wallet);
               },
-              style: "padding:3px",
+              style: "padding:5px",
             },
             "Connect"
           )
@@ -39,12 +41,12 @@ export function openConnectModal() {
   van.add(
     document.body,
     Modal(
-      { closed, blurBackground: true },
+      { closed, blurBackground: true,modalStyleOverrides:{"padding":'26px',"border-radius":'12px'} },
       formDom,
       p(
         { style: "display: flex; justify-content: space-evenly;" },
         button(
-          { onclick: () => (closed.val = true), style: "padding:3px 25px;" },
+          { onclick: () => (closed.val = true), style: "padding:5px 25px;font-size:15px;" },
           "Cancel"
         )
       )
