3a330d39adec4f1e91ddcedecc51ea66918e9dab
clark-cui <cuirongchuan@163.com>
2024-11-12 16:22:19 +0800

    fix: getUserPlatform issue

--- packages/dapp-connect-sdk/src/utils/platform.ts
+++ packages/dapp-connect-sdk/src/utils/platform.ts
@@ -1,13 +1,14 @@
 import { UserPlatform } from "../types";
 
-export const isTelegram = (userAgent: string): boolean =>
-  /Telegram/i.test(userAgent);
+//https://stackoverflow.com/questions/78024558/with-javascript-code-how-to-detect-if-user-is-using-my-website-using-telegram-we
+export const isTelegram = (): boolean =>
+  typeof window?.TelegramWebviewProxy !== "undefined";
 
 export const isMobile = (userAgent: string): boolean =>
   /Android|iPhone|Mobile/i.test(userAgent);
 
 export const getUserPlatform = (userAgent: string): UserPlatform => {
-  if (isTelegram(userAgent)) return UserPlatform.TELEGRAM;
+  if (isTelegram()) return UserPlatform.TELEGRAM;
   if (isMobile(userAgent)) return UserPlatform.MOBILE_BROWSER;
   return UserPlatform.PC_BROWSER;
 };
