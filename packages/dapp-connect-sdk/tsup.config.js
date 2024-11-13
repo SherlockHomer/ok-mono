0bdb49ba079fe4ebfd8c5342ad1dbb265aa95a51
shaowei.zhou <shaowei.zhou@okg.com>
2024-11-13 16:15:24 +0800

    build: use esBuild

--- packages/dapp-connect-sdk/tsup.config.js
+++ packages/dapp-connect-sdk/tsup.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  // dts: true,
-  splitting: false,
+  // dts: true, // ts 代码不严格，存在 error 时无法使用
+  splitting: true,
   sourcemap: true,
   clean: true,
   format: ["esm", "cjs"],
   treeshake: true,
   legacyOutput: true,
+  minify: false,
+  bundle: true,
+  external: [], // 将 node_modules 包含在打包中
+  noExternal: [/(.*)/],
 });
