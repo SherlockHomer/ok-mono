ae40a70af26e9a88e6f49283b9b555103aa9e9c7
Elvis Wong <elvis.wong@okg.com>
2024-11-14 15:48:19 +0800

    Open tg mini wallet directly when connect modal in TG env

--- packages/dapp-connect-sdk/src/adapters/ethereumAdapter.ts
+++ packages/dapp-connect-sdk/src/adapters/ethereumAdapter.ts
@@ -71,6 +71,10 @@ class EthereumAdapter extends BaseAdapter {
     return false;
   }
 
+  public async disconnect() {
+    this.okxUniversalProvider.disconnect();
+  }
+
   public eventCallbackHandlers: Record<string, Set<Function>> = {
     connect: new Set(),
     disconnect: new Set(),
